47ad6834c0a6833b4607c5b6acff816aea73aead
Genki Hagata <ghagata@hugeinc.com>
2016-09-27 22:11:28 -0700

    🍹 Adds asset handling task cleans up watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 
 const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
 const nunjucks = require('gulp-nunjucks-render');
 const pump = require('pump');
 const sass = require('gulp-sass');
@@ -48,17 +49,29 @@ gulp.task('scripts', function(cb) {
   );
 });
 
+gulp.task('copy', ['clean'], function() {
+  return gulp.src(['source/assets/**/*'], {
+    base: 'source'
+  }).pipe(gulp.dest('_build'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('_build/assets', {read: false})
+      .pipe(clean());
+});
 
 gulp.task('watch', function() {
-  gulp.watch('./source/styles/**/*.scss', ['sass']);
-  gulp.watch('./source/pages/**/*.tpl', ['templates']);
-  gulp.watch('./source/scripts/**/*.js', ['scripts']);
+  gulp.watch('source/styles/**/*.scss', ['sass']);
+  gulp.watch(['source/pages/**/*.tpl','source/templates**/*.tpl'], ['templates']);
+  gulp.watch('source/scripts/**/*.js', ['scripts']);
+  gulp.watch('source/assets/**', ['copy']);
 });
 
 gulp.task('default', [
   'sass',
   'templates',
   'scripts',
+  'copy',
   'serve',
   'watch'
 ]);
