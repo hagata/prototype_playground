8c1a4ad27b16882cab98d222b5aba54fb3300f9e
Jessie Kim <jekim@hugeinc.com>
2016-10-24 11:40:40 -0700

    adds autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 
+const autoprefixer = require('gulp-autoprefixer');
 const browserify = require('browserify');
 const browserSync = require('browser-sync').create();
 const buff = require('vinyl-buffer');
@@ -90,6 +91,7 @@ gulp.task('sass', function() {
   return gulp.src("source/styles/**/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest("_build/styles"))
     .pipe(browserSync.stream());
